c101455f3ef4c101f5e2df976b80d492d69d85f5
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-12 23:58:44 +0100

    new commits

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter, Actor, Roboto } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/Navbar"
+import Navbar from "../../src/components/Navbar"
 const inter = Inter({ subsets: ["latin"] });
 const actor = Actor({ subsets: ["latin"], weight: ["400"] });
 const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400"] });
